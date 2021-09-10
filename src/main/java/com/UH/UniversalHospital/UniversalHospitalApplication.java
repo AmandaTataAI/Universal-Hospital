f01aa5c6d9aef1c04cbd3511c63b96f9b0efd608
Amanda Tata <imoamandatata@gmail.com>
2021-09-10 17:10:24 +0300

    Universal Hospital

--- src/main/java/com/UH/UniversalHospital/UniversalHospitalApplication.java
+++ src/main/java/com/UH/UniversalHospital/UniversalHospitalApplication.java
@@ -2,9 +2,10 @@ package com.UH.UniversalHospital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UniversalHospitalApplication {
+public class UniversalHospitalApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(UniversalHospitalApplication.class, args);
--- src/main/java/com/UH/UniversalHospital/web/IndexController.java
+++ src/main/java/com/UH/UniversalHospital/web/IndexController.java
@@ -1,4 +1,4 @@
-package com.UH.UniversalHospital;
+package com.UH.UniversalHospital.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
